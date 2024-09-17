c9fdba1fd1402c4a7dfa0b0c92d66f214dbbdce6
nickmarietta <nick.marietta@csu.fullerton.edu>
2024-09-17 09:03:48 -0700

    first push, all tests passed and committed

--- fib.c
+++ fib.c
@@ -1,7 +1,59 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-int main(int argc, char* argv[]) {
-   printf("Hello!\n");
+int fibonacciIterative(unsigned long long int n) {
+   // the sums of the terms preceding are the current
+   if (n == 1) {
+      printf("0");
+      return 0;
+   } else if (n == 2) {
+      printf("1");
+      return 0;
+   }
+   // F(1) is firstPrevious and F(2) is secondPrevious
+   unsigned long long int firstPrevious = 0;
+   unsigned long long int secondPrevious = 1;
+   unsigned long long int fibonacciSum = 1;
+
+   // start for loop from 3 since if covers the other base cases
+   for (unsigned long long int i = 3; i <= n; i++) {
+      fibonacciSum = firstPrevious + secondPrevious;
+      firstPrevious = secondPrevious;
+      secondPrevious = fibonacciSum;
+   }
+   return fibonacciSum;
+}
+
+
+
+int fibonacciRecursive(unsigned long long int n) {
+   // indexing was off at first since Fibonacci number 1 is 0
+   if (n <= 1) {
+      return 0;
+   } else if (n == 2) {
+      return 1;
+   }
+   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+int main(int argc, char *argv[]) {
+   // assign command line arguments to appropriate variables
+   //unsigned long long int firstFibNumber;
+   int firstFibNumber;
+   //char *endptr;
+   //firstFibNumber = strtoull(argv[1], &endptr, 10);
+   firstFibNumber = atoi(argv[1]);
+   char method = argv[2][0];
+
+      
+   if (method == 'i') {
+      // here do the iterative method
+      //printf("This is the Iterative method up to: %d\n", fibonacciNum);
+      printf("%d", (fibonacciIterative(firstFibNumber)));
+   } else {
+      // here do the recursive method
+      //printf("This is the Recursive method up to: %d\n", fibonacciNum);
+      printf("%d", (fibonacciRecursive(firstFibNumber)));
+   }
    return 0;
 }
